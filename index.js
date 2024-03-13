392ebcd784c12b9281bc7f96ad13f7dc9817629f
ShametovKuanysh <shametovkuanysh0@gmail.com>
2024-03-13 16:58:52 +0500

    Done HashSet

--- index.js
+++ index.js
@@ -72,6 +72,79 @@ class HashMap {
     }
 }
 
+class HashSet {
+    #buckets
+    #loadfactor
+    constructor() {
+        this.#buckets = []
+        this.#buckets.length = 8
+        this.#loadfactor = 0
+    }
+
+    hash = (key) => {
+        let hashCode = 0;
+      
+        const primeNumber = 41;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#buckets.length;
+        }
+
+        return hashCode;
+    }
+
+    set = (key) => {
+        const hash = this.hash(key)
+        if (0.75 < this.#loadfactor / this.#buckets.length ){
+            this.increase()
+        }
+        this.#buckets[hash] = key
+        this.#loadfactor++
+    }
+
+    increase = () => {
+        this.#buckets.length = this.#buckets.length * 2
+    }
+
+    get = (key) => {
+        const hash = this.hash(key)
+        return this.#buckets[hash] ? this.#buckets[hash] : null
+    }
+
+    has = (key) => {
+        const hash = this.hash(key)
+        return this.#buckets[hash] ? true : false
+    }
+
+    remove = (key) => {
+        const hash = this.hash(key)
+        if (this.#buckets[hash]) {
+            this.#buckets[hash] = null 
+            return true
+        }
+        return false
+    }
+
+    length = () => {
+        console.log(this.#buckets.length)
+        return this.#buckets.filter(data => data).length
+    }
+
+    clear = () => {
+        this.#buckets = []
+    }
+
+    keys = () => {
+        return this.#buckets.filter(data => {return data})
+    }
+
+    entries = () => {
+        return this.#buckets.flat()
+    }
+}
+
+
+
+
 const hh = new HashMap()
 
 console.log(hh.length())
@@ -87,3 +160,20 @@ console.log(hh.set('aaaxzas', 123))
 console.log(hh.set('aaawqeas', 234))
 console.log(hh.entries())
 console.log(hh.length())
+
+
+const hs = new HashSet()
+
+console.log(hs.length())
+console.log(hs.set('KK'))
+console.log(hs.set('qqq'))
+console.log(hs.set('wwww'))
+console.log(hs.set('eeee'))
+console.log(hs.set('rrrrr'))
+console.log(hs.set('tttttt'))
+console.log(hs.set('yyyyyyy'))
+console.log(hs.set('uuuuuuuu'))
+console.log(hs.set('aaaxzas'))
+console.log(hs.set('aaawqeas'))
+console.log(hs.entries())
+console.log(hs.length())
